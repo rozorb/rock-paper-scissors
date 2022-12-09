e715882c08e850c0f1d9ad7412755f34e4d16640
Ron <ronnierozo@yahoo.com>
2022-12-08 19:23:20 -0500

    connected playAGame to btn event listener

--- script.js
+++ script.js
@@ -40,17 +40,17 @@ function game() {
     let lose = 'Lose';
     let winCount = 0, loseCount = 0;
 
-   // for (var i = 0; i < 5; i++) {
-        let playerChoice = prompt("What is your choice?").toLowerCase();
-        sentence = playRound(playerChoice, getComputerChoice());
+   for (var i = 0; i < 5; i++) {
+       let playerChoice = prompt("What is your choice?").toLowerCase();
+       sentence = playRound(playerChoice, getComputerChoice());
         console.log(sentence);
         if(sentence.includes(win)) {
             winCount++;
         } else if(sentence.includes(lose)) {
             loseCount++;
-        }
-
-   // }
+        } 
+    
+    }
 
     if(winCount > loseCount) {
         return 'Congrats you won!';
@@ -62,4 +62,43 @@ function game() {
 
 }
 
-console.log(game());
+/*document.body.addEventListener("click", event => {
+    if (event.target.nodeName == "BUTTON") {
+    console.log("Clicked", event.target.textContent);
+    console.log(playRound(event.target.textContent, getComputerChoice()));
+    }
+}); */
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(btn => btn.addEventListener('click', playAGame));
+
+function playAGame (e) {
+    console.log(e.target.id);
+    let player = String(e.target.id);
+    let computer = getComputerChoice();
+    console.log("inside computer", computer);
+    //conditions deciding who wins round;
+    if(player === computer) {
+        console.log('There is no winner!');
+        return 'There is no winner!';
+    } else if (player === 'rock' && computer === 'paper') {
+        console.log('You Lose! Paper beats Rock');
+        return "You Lose! Paper beats Rock";
+    } else if (player === 'rock' && computer === 'scissors') {
+        console.log('You Win! Rock beats Scissors');
+        return "You Win! Rock beats Scissors"
+    } else if (player === 'paper' && computer === 'rock') {
+        console.log('You Win! Paper beats Rock');
+        return "You Win! Paper beats Rock";
+    } else if (player === 'paper' && computer === 'scissors') {
+        console.log('You Lose! Scissors beats Paper');
+        return "You Lose! Scissors beats Paper"
+    } else if (player === 'scissors' && computer === 'paper') {
+        console.log('You win! Scissors beats Paper');
+        return "You win! Scissors beats Paper";
+    } else if (player === 'scissors' && computer === 'rock') {
+        console.log('You Lose! Rock beats Scissors');
+        return "You Lose! Rock beats Scissors"
+    } 
+}
+//console.log(game());
